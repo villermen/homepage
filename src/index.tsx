d5bcd2f2bd5fdd971badb0d861e4d8f6343f6cdb
Villermen <villermen@gmail.com>
2025-05-07 08:29:59 +0200

    Remove flash of unsupported text while loading

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import 'assets/main.css';
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 
-createRoot(document.querySelector('.react-root')).render(<Layout />);
+createRoot(document.querySelector('#react-root')).render(<Layout />);
